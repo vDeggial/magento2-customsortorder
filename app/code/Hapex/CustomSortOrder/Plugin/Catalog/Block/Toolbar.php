f9b1dd3cf40e9f59bd7325a90ac689a5c1c755b4
Oleg K <voknevark@gmail.com>
2020-08-14 14:05:25 -0700

    Update Toolbar.php

--- app/code/Hapex/CustomSortOrder/Plugin/Catalog/Block/Toolbar.php
+++ app/code/Hapex/CustomSortOrder/Plugin/Catalog/Block/Toolbar.php
@@ -21,39 +21,49 @@ class Toolbar
                 try {
                     $currentOrder = $toolbar->getCurrentOrder();
                     $currentDirection = $toolbar->getCurrentDirection();
-                    switch ($currentOrder) {
-
-                        case 'qty_asc':
-                            $this->_collection->setOrder('quantity_and_stock_status', "asc");
-                            break;
-
-                        case 'qty_desc':
-                            $this->_collection->setOrder('quantity_and_stock_status', "desc");
-                            break;
-
-                        case 'price_asc':
-                            $this->_collection->setOrder('price', "asc");
-                            break;
-                        case 'price_desc':
-                            $this->_collection->setOrder('price', "desc");
-                            break;
-
-                        case 'name_asc':
-                            $this->_collection->setOrder('name', "asc");
-                            break;
-                        case 'name_desc':
-                            $this->_collection->setOrder('name', "desc");
-                            break;
-
-                        default:
-                            $this->_collection->setOrder($currentOrder, $currentDirection);
-                            break;
-                    }
+                    $this->setOrder($currentOrder, $currentDirection);
                 } catch (\Exception $e) {
                     $this->helperData->errorLog(__METHOD__, $e->getMessage());
                 }
                 break;
         }
         return $result;
+    }
+
+    private function setOrder($currentOrder = null, $currentDirection = null)
+    {
+        switch ($currentOrder) {
+
+            case 'qty_asc':
+                $this->setCurrentOrder('quantity_and_stock_status', "asc");
+                break;
+
+            case 'qty_desc':
+                $this->setCurrentOrder('quantity_and_stock_status', "desc");
+                break;
+
+            case 'price_asc':
+                $this->setCurrentOrder('price', "asc");
+                break;
+            case 'price_desc':
+                $this->setCurrentOrder('price', "desc");
+                break;
+
+            case 'name_asc':
+                $this->setCurrentOrder('name', "asc");
+                break;
+            case 'name_desc':
+                $this->setCurrentOrder('name', "desc");
+                break;
+
+            default:
+                $this->setCurrentOrder($currentOrder, $currentDirection);
+                break;
+        }
+    }
+
+    private function setCurrentOrder($order = null, $direction = null)
+    {
+        $this->_collection->setOrder($order, $direction);
     }
 }
