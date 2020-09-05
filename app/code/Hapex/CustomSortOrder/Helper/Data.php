b74729db1a48f9052a227fba848b84e07c6de986
Oleg K <voknevark@gmail.com>
2020-09-05 09:22:55 -0700

    Update Helper\Data.php

--- app/code/Hapex/CustomSortOrder/Helper/Data.php
+++ app/code/Hapex/CustomSortOrder/Helper/Data.php
@@ -3,17 +3,11 @@
 namespace Hapex\CustomSortOrder\Helper;
 
 use Hapex\Core\Helper\DataHelper;
-use Magento\Framework\App\Helper\Context;
-use Magento\Framework\ObjectManagerInterface;
 
 class Data extends DataHelper
 {
     protected const XML_PATH_CONFIG_ENABLED = "hapex_customsortorder/general/enable";
     protected const FILE_PATH_LOG = "hapex_sortorder";
-    public function __construct(Context $context, ObjectManagerInterface $objectManager)
-    {
-        parent::__construct($context, $objectManager);
-    }
 
     public function isEnabled()
     {
